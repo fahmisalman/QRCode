889cbe9855981b04978a8798b46fbe0425ce9660
fahmisalman <fahmisalman@gmail.com>
2019-01-24 13:44:37 +0700

    Fix QR.py

--- Model/QR.py
+++ Model/QR.py
@@ -7,11 +7,14 @@ class QRCode(object):
     def __init__(self):
         self.filename = 'Output'
         self.link = ''
-        self.qr = ''
+        self.qr = None
         pass
 
     def generate_qrcode(self):
         return pyqrcode.create(self.link)
+
+    def save_png(self):
+        self.qr.png('{}/Output/{}.png'.format(os.getcwd(), self.filename), scale=8)
 
     def save_svg(self):
         self.qr.svg('{}/Output/{}.svg'.format(os.getcwd(), self.filename), scale=8)
@@ -19,9 +22,11 @@ class QRCode(object):
     def save_eps(self):
         self.qr.eps('{}/Output/{}.eps'.format(os.getcwd(), self.filename), scale=1)
 
-    def fit(self, link, svg=False, eps=False, show=False):
+    def fit(self, link, svg=False, eps=False, png=False, show=False):
         self.link = link
         self.qr = self.generate_qrcode()
+        if png:
+            self.save_png()
         if svg:
             self.save_svg()
         if eps:
